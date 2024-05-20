c3ba65fae56dfe8f73d2a338176bde1e39fcb077
aqsa.qadir44@gmail.com <aqsa.qadir44@gmail.com>
2024-05-20 17:49:53 +0500

    Hide the key

--- todo/database.py
+++ todo/database.py
@@ -3,8 +3,10 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from starlette.config import Config
+from starlette.datastructures import Secret
 
-SQLALCHEMY_DATABASE_URL = "postgresql://todo_db_owner:uGmrQH2W9vTU@ep-divine-bonus-a52mlcod.us-east-2.aws.neon.tech/todo_db?sslmode=require"
+SQLALCHEMY_DATABASE_URL= Config("SQLALCHEMY_DATABASE_URL", cast=Secret)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL , pool_recycle=300)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
